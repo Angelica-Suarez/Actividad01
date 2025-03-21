9b59223d521c94e01cda468cd7380b2d85277277
SCPC 515 <SCPC515@labs.upds>
2025-03-20 20:24:16 -0400

    se actualizo el archivo actua

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CardproductoComponent } from './components/cardproducto/cardproducto.component';
+import { CardproductosComponent } from './components/cardproductos/cardproductos.component';
+import { CardbuscadorComponent } from './components/cardbuscador/cardbuscador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'acercade', component: AboutComponent },
+  { path: 'productos', component: CardproductosComponent },
+  { path: 'producto/:id', component: CardproductoComponent},
+  { path: 'buscar/:termino', component: CardbuscadorComponent},
+
+];
+  
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/sevices/productos.service.ts
+++ src/app/sevices/productos.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Producto } from '../models/Productos.modesl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  constructor() { 
+    console.log("Servicio listo para usar!!!");
+  }
+  private productos: Producto[] =[
+    {nombre: "Audifom¡nos", img: "assets/img/1audi.avif" ,precio: 100},
+    {nombre: "Tenis", img: "assets/img/2tenis.avif" ,precio: 100},
+    {nombre: "Reloj", img: "assets/img/3reloj.avif" ,precio: 100},
+  ];
+  getProductos(): Producto[] {return this.productos;}
+  getProducto(id: number) {return this.productos[id];}
+  buscarProductos(termino: string): Producto[] {
+    let productosArr: Producto[] = [];
+    termino = termino.toLowerCase();
+    for (let i = 0; i < this.productos.length; i++){
+      let producto = this.productos[i];
+      let nombre = producto.nombre.toLowerCase();
+      if (nombre.indexOf(termino) >= 0){
+        productosArr.push(producto)
+      }
+    }
+    return productosArr;
+  }
 }
